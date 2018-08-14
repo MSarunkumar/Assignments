e16ac0d7e21839f8ce2cbb464479b05104dfd76a
MSarunkumar <41627388+MSarunkumar@users.noreply.github.com>
2018-08-14 19:25:07 +0530

    Update Assi_JavaScript_Validation.js

--- Assi_JavaScript_Validation.js
+++ Assi_JavaScript_Validation.js
@@ -1,5 +1,7 @@
-      var result;
-       function formValidation() 
+     var result;
+	  var Valid=false;
+	  var password='';
+    /*   function formValidation() 
          {
               
                   var firstname     =document.getElementById("fname");
@@ -16,110 +18,291 @@
                   var state         =document.getElementById("selects");
                   var country       =document.getElementById('selectc');
                   var subscribes    =document.getElementsByName('subscribe');
-                  
-                
-//......................................................................................first name
-             if(firstname.value == "")
-              {
-                alert("Enter the your first name - can`t vacant... ok");
-                return false;
-                }
-//......................................................................................last name
-                if(lastname.value== "")
-                {
-                  alert("Enter the your last name - can`t vacant... ok");
-                   return false;
-                }
-//......................................................................................Email
-
-                if(email == ""){
-                  alert("Enter the your Email - can`t vacant... ok");
-                   return false;
+				  if(Valid==true) return true;
+				  else            return false;
+				  
+		 } 
+                   
+//...................................................           first name Validation 
+     
+	 /*   function firstNameValid()
+            {
+				
+			       var firstname=document.getElementById("fname").value;
+			       var reg = (/[@|#|!|$|%|^|*|(|)|-|=|_|&|.|<|>|?|:|;|'|"]/g);
+			      
+			     if(firstname =="")
+                  {  
+                         ShowError("fid",' Please Enter the your first name.   ');
+                         Valid=false;
+                    }
+					
+				 else if(firstname.trim()=='')
+				   { 
+				         ShowError("fid", '  Please Don`t Enter spaces.    ');	
+				         Valid=false;
+				   }
+				
+				 else if((/[0-9]/.exec(firstname))!=null ){
+                         ShowError("fid", 'Please Don`t Enter digit in first name.   ');	   
+                         Valid=false;				 			
+				    }
+			     else if((firstname).match(reg)!=null ){
+                         ShowError("fid" ,'     Please Don`t Enter special character in first name.    ');	   
+                         Valid=false;				 			
+				    }
+				 else if(firstname.length<3 || firstname.value.length>25){
+					     ShowError("fid" ,'  Please Enter your first name in [2-25] lenght ');
+				         Valid=false;
+					 }
+				 else if(getSpace(firstname)> 0)
+				 {
+					     ShowError("fid" ,'  Please Don`t Enter any spaces in First name  ');
+				         Valid=false;
+				     }
+				 else{
+					  document.getElementById("fid").innerHTML='';
+				          Valid=true;
+						  }
+				
+			}
+			
+//...................................................           last name Validation 
+      
+	  function lastNameValid()
+            { 
+			      var lastname=document.getElementById("lname");
+			      var reg=(/[@|#|!|$|%|^|*|(|)|-|=|_|&|.|<|>|?|:|;|'|"]/g);
+			      
+			     if(lastname.value =="")
+                  {  
+                         ShowError("lid",' Please Enter the your last name.   ');
+                         Valid=false;
+                    }
+					
+				 else if(lastname.value.trim()=='')
+				   { 
+				         ShowError("lid", '  Please Don`t Enter spaces in last name    ');	
+				         Valid=false;
+				   }
+				
+				 else if((/[0-9]/.exec(lastname.value))!=null ){
+                         ShowError("lid", 'Please Don`t Enter digit in last name.   ');	   
+                         Valid=false;				 			
+				    }
+			     else if((lastname.value).match(reg)!=null ){
+                         ShowError("lid" ,'     Please Don`t Enter special character in last name.    ');	   
+                         Valid=false;				 			
+				    }
+				 else if(lastname.value.length<3 || lastname.value.length>25){
+					     ShowError("lid" ,'  Please Enter your last name in [2-25] length ');
+				         Valid=false;
+					 }
+				 else if(getSpace(lastname.value)>0)
+				 {
+					     ShowError("lid" ,'  Please Don`t Enter any spaces in last name  ');
+				         Valid=false;
+				     }
+				 else{
+					  document.getElementById("lid").innerHTML='';
+				          Valid=true;}
+				
+				
+			}   
+//......................................................................................Email Validation 
+      function emailValid()
+	  {
+		  
+		       var email =document.getElementById("email").value;
+		      // var reg=/[a-z0-9A-Z]/g/;
+		       var regcopy=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+               
+			   if(email == ""){
+                  ShowError("eid" ," Please Enter the your Email ");
+                    Valid=false;
                     }
 
-                else{
-                  
-                    var at = email.indexOf("@");
-                    var dot = email.lastIndexOf(".");
-                    if (at<1 || dot<at+2 || dot+2>=email.length) {
-                             alert(" !!!... Formate is not currect of email  ...!!!");
-                                return false;
-                       }
-                     }
-//...........................................................................................................password
+                else if(getSpace(email)>0) {
+					ShowError("eid" ," Please Don`t Enter spaces in Email ");
+                    Valid=false;
+				}
+				 else if(email.length< 5 ) {
+					ShowError("eid" ," Please Enter some more charecter in Email ");
+                    Valid=false;
+				}
+				else if(email.length>50)
+				{
+					ShowError("eid" ," Please Enter less than 50  charecter in Email ");
+					 Valid=false;
+				 }
+				
+				
+                     
+   }
+//...........................................................................        password Validation 
+  */function passwordValid()
+      {
+	       password  =document.getElementById('password').value;
+		  //var reg=/[a-z0-9A-Z!@#$%^&*()_+=<>.,?;:'"]/g/;   
+			var regCopy=  /^[a-zA-Z0-9!@#$%^&*]{8,15}$/;  
+			 
+			   if(password== '')
+			  {
+				   ShowError("pid" , " Please Enter the password ");
+                   Valid= false;
+				}
+			  else if(password.length < 8)
+              {
+                 ShowError("pid" , "Password Lenght should be minimum 8 characters ");
+                  Valid= false;
+              }
+			   else if(password.length > 15)
+              {
+                 ShowError("pid" , "Password Lenght should be maximum 15 characters ");
+                  Valid= false;
+              }
+			  
+			  else if(!(regCopy.test(password)))
+			  {  
+		            ShowError("pid" , "Password must be contain 1 lowercase 1 uppercase 1 digit 1 special char");
+                    Valid= false;
+				 }
+			   else 
+			   {
+				   document.getElementById("pid").innerHTML='';
+				          Valid=true;
+						  }   
+               
+	   }
 
-              if(password.value.length < 8)
+//.....................................................................  confirm - password 	   
+
+     /*   function cpasswordValid()
+         {
+           var cpassword =document.getElementById('cpassword').value;
+				  
+			if(password != cpassword)
               {
-                alert("!!!...  Password Lenght should be minimum 8 characters  ...!!!");
-                return false;
-              }
-//...........................................................................................................conform password
-                      
-              if(password.value != cpassword.value)
-              {
-                alert("!!!...  The Confirm-Password did not match  ...!!!");
-                return false;
-              }
-//...................................................................................... gender 
-               if (! (genders[0].checked || genders[1].checked|| genders[2].checked) ) {
+                 ShowError("cpid","  The Confirm-Password did not match  ");
+                 Valid=false;
+              } 
+              				 
+		 }
+			  
+			 
+	  
+			  
+			  
+			  
+			  
+  
+
+             
+  } 
+//...................................................................................... gender Validation
+    /* function genderValid(){           
+		   if (! (genders[0].checked || genders[1].checked|| genders[2].checked) ) {
                   alert("!!!...   Select at least one gender   ...!!!");
                   return false;
                }
-//............................................................................................................mobile1
-              
-                    if(mobile1.value==""){
-                        alert("!!!...   Please enter the Contact number   ...!!!");
-                           return false;
+	 }*/
+//.......................................................................................  mobile1 Validation
+  /*             function mobile1Valid()
+			    {
+				    // alert('hii   la la la');
+					 
+					var mobile1 =document.getElementById('mob1').value;
+					//var reg=/   /;
+					
+					
+                    if(mobile1 === ""){
+                        ShowError("mid1"," Please enter the Contact number  ");
+                           Valid= false;
                              }
                          
-                     if(mobile1.value.length!=10)
+                     else if(mobile1.length != 10)
                         {
-                           alert("!!!...   Enter only 10 digits   ...!!!"); 
-                            return false;
+                           ShowError("mid1","  Please  Enter 10 digits  "); 
+                            Valid= false;
                         }
                     
 
-                     if(isNaN(mobile1.value))
-                      {   alert("!!!...   Please enter only digits   ...!!!");
-                          return false;
+                     else if(isNaN(mobile1))
+                      {   ShowError("mid1","   Please enter only digits   ");
+                          Valid=false;
                          }
-//............................................................................................................mobile 2
+					 else if(mobile1=='0000000000')
+					 {
+						ShowError("mid1","   Please Enter Valid phone number    ");
+					    Valid=false;
+					 }						
+					 else 
+					 {
+						  document.getElementById("mid1").innerHTML='';
+				          Valid=true;}
+						  
+						 
+				}   
+//.......................................................................................  mobile2 Validation
+           
 
-              if(mobile2.value==""){
-                        alert("!!!...   Please enter the Contact number  mobile 2 ...!!!");
-                           return false;
+
+
+		     function mobile2Valid(){
+				  
+				   var mobile2   =document.getElementById('mob2').value;
+			
+                   if(mobile2 != "")
+				       {
+                       
+					   if( isNaN(mobile2))
+                            {    ShowError("mid2" ,"   Please enter only digits in phone number  mobile 2 ");
+                              Valid=false;
                              }
-
-
-                     if(mobile2.value.length!=10)
-                        {
-                           alert("!!!...   Enter only 10 digits mobile 2  ...!!!"); 
-                            return false;
-                        }
+					     
                     
 
-                     if( isNaN(mobile2.value))
-                      {   alert("!!!...   Please enter only digits  mobile 2 ...!!!");
-                          return false;
-                         }
+                          else if(mobile2.length!=10)
+                         {
+                             ShowError("mid2", " Enter only 10 digits in Phone number 2 "); 
+                             Valid =false;
+                           }
+						    else if(mobile2=='0000000000')
+					            {
+						                ShowError("mid2","   Please Enter Valid phone number 2   ");
+					                   Valid=false;
+					                   }			
+					      else 
+					        {
+						      document.getElementById("mid2").innerHTML='';
+				              Valid=true;}
+	                    }
+			  }             
 //............................................................................................................address 1
-                      if(address1.value== "")
+                /*  function address1Valid(){
+				   if(address1.value== "")
                        {
                         alert("!!!... You have to enter address  - can`t vacant... ok");
                         return false;
                        }
+				  }
 //............................................................................................................address 2
-                      if(isNaN(address2.value))
+                   function address2Valid(){                     
+					 if(isNaN(address2.value))
                        {
                         alert("!!!...  address 2 can`t be only digit  -  ok");
                         return false;
                        }
+				   }
 //............................................................................................................city
-                      if(City1.value== "")
+                       function cityValid(){
+
+					 if(City1.value== "")
                        {
                         alert("!!!... You have to enter city  - can`t vacant... ok");
                         return false;
                        }
+					   }
 //............................................................................................................state 
 //.............................................................................................................country
 //........................................................................................subscribe New
@@ -149,8 +332,10 @@
                  return false;
                  }
              
- }
+        
 
+//...........   It is genrating two numbers, operand and calling  calc fuction get a actual result of captcha 
+		
 function refresh()
   {
                var numberOne = parseInt(Math.floor(Math.random() * 10));
@@ -166,6 +351,9 @@ function refresh()
              
 
       }
+	  
+ //.......    It will performe calculation (two number and operands ) and return result.
+ 
  
 function calc(firstnumber,lastnumber,opration){
             var result1;
@@ -193,5 +381,28 @@ switch(opration) {
 
     return(result1);
    }
+   */
+   
+   //.......   It will return total no of spaces present in String 
+   function getSpace(str)
+   {
+	   var count=parseInt(0);
+	   for(var i=0;i< str.length;i=i+1)
+	   {
+		   if(str[i]==' ')
+			   count=count+1;
+	   }
+        return count;	   
+	   
+   }
+   
+   //.............  ShowError(id,msg)
+         function ShowError(ids,msg)
+		 {
+			  document.getElementById(ids).innerHTML= msg; 
+		 }
+   //................ check password formate return true if currect else return false             			 
+		 
+   
 //................................................................................................................
   
