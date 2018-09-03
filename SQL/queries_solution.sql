c016a414404c9afadfb0beb134a82b2306966fe0
MSarunkumar <41627388+MSarunkumar@users.noreply.github.com>
2018-09-03 11:29:02 +0530

    Update queries_solution.sql

--- SQL/queries_solution.sql
+++ SQL/queries_solution.sql
@@ -1,4 +1,4 @@
-/* For find the data*/
+/*............       For find the data    ...........*/
 SELECT Table_Name, Column_Name 
 FROM  INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_CATALOG = 'AdventureWorks2012_Data'
@@ -13,10 +13,10 @@ c. Total distinct ntLevel
 
 */
 
-SELECT COUNT(ntEmpid)AS Total_Employee,
-       COUNT(DISTINCT(ntlevel)) AS Distict_ntLevel,
-       MIN(moSalary)AS Minimum_Salary
-FROM tblemp;
+SELECT  COUNT(ntEmpid)AS Total_Employee,
+        COUNT(DISTINCT(ntlevel)) AS Distict_ntLevel,
+        MIN(moSalary)AS Minimum_Salary
+FROM        tblemp;
 
 
 /* query 2.
@@ -27,13 +27,12 @@ FROM tblemp;
         
 */
 
-SELECT [ntEmpID], tblemp.[vcName],tblEmp.[vcMobieNumer]
-    FROM tblEmp ;
+SELECT e.[ntEmpID], e.[vcName],e.[vcMobieNumer]
+FROM   tblEmp e ;
  
-  /* OR  */
-
-  SELECT [ntEmpID], E.[vcName],E.[vcMobieNumer]
-    FROM tblEmp E;  
+  /* GOOD ONE */
+SELECT [ntEmpID], [vcName],[vcMobieNumer]
+FROM   tblEmp ;  
 
 /* query 3.
 
@@ -43,36 +42,55 @@ SELECT [ntEmpID], tblemp.[vcName],tblEmp.[vcMobieNumer]
      
      */
 	 
-	IF((SELECT vcMobieNumer FROM tblEmp WHERE vcMobieNumer is NULL 
-	   AND  ntlevel=1) is NULL )
-	  SELECT * FROM tblemp WHERE vcMobieNumer is NULL AND  ntlevel=1 ;
-	 ELSE 
-	      SELECT *  FROM tblemp WHERE ntlevel=0;
-	  
+	 SELECT * FROM tblemp
+	 where 1 = (
+	         SELECT   
+	           CASE   
+	             WHEN  (ntlevel=1 AND vcMobieNumer IS NULL) THEN '1'
+		         WHEN    ntlevel=0  THEN '1'
+		       ELSE '0'
+		      END)
+		  
 /* query 4. 
 Write a sql query which displays those employee data first, who knows javAScript.
  ............  complete.....
  */
 SELECT  * 
-FROM    tblemp 
-WHERE   vcskills LIKE '%JavAScript%' 
-UNION 
-SELECT  * 
-FROM    tblemp 
-WHERE   vcskills not LIKE '%JavAScript%' ;
-
+FROM     tblemp
+ORDER BY (CASE
+             WHEN vcskills LIKE '%JavAScript%' THEN ntLevel
+             ELSE ntEmpID
+            END);
 
 /* query 6.
-Group by clause work in grouping so mobile no. will not group with same name.
+When I executed this query:
+           
+   SELECT [vcName],[vcMobieNumer] FROM [dbo].[tblEmp] GROUP BY [vcName]
+           
+    I got following error message:
+    Column 'dbo.tblEmp.vcMobieNumer' is invalid in the select list because it is not contained in either an aggregate function or the GROUP BY clause.
 
-solutiON 1:*/
+    Can you explain above error message? Write at least two possible solutions. 
 
-SELECT vcName, vcMobieNumer 
-FROM tblemp 
-WHERE  vcname in (
-                   SELECT   vcname 
-                   FROM     tblemp 
-                   GROUP BY vcName);
+...>>>>>    Group by clause work in grouping so mobile no. will not be group with same name.
+            and this table have one to many relationship b\t name and mobile
+
+.............     solutiON 1:........*/
+SELECT  vcName 
+FROM    tblEmp
+GROUP BY  vcName ;
+ 
+/*..........    solution 2  .............. */ 
+
+SELECT vcMobieNumer 
+FROM   tblemp 
+GROUP BY  vcMobieNumer ; 
+
+/*............. solution 3  ......................*/
+
+SELECT    vcName ,COUNT(vcMobieNumer) AS total_mobiles 
+FROM      tblemp  
+GROUP BY  vcName; 
 
 
 
@@ -80,46 +98,62 @@ WHERE  vcname in (
 Write a sql query to get the ntLevel of the employees getting salary greater than 
 average salary. */
 
-use MyDataBASe;
+USE MyDataBASe;
 
-SELECT  ntlevel, vcname 
+SELECT  ntlevel, vcname      
 FROM    tblemp 
-WHERE   mosalary >(SELECT AVG(mosalary)FROM tblemp);
+WHERE   mosalary >(SELECT AVG(mosalary)
+                   FROM   tblemp);
 
 /*     Query 8.
-Write a query to get the count of employees with a valid Suffix */
+Write a query to get the count of employees with a valid Suffix       * /
 
-use AdventureWorks2012_Data;
-SELECT  COUNT(*) AS total_valid_suffix_employee 
+USE AdventureWorks2012_Data;
+SELECT  COUNT(BusinessEntityID) AS total_valid_suffix_employee 
 FROM    PersON.PersON 
-WHERE   Suffix is not NULL;
+WHERE   Suffix IS NOT NULL;
 
 /*Query 9. 
 Using BusinessEntityAddress table (AND other tables AS required), list the full name of people living in the state of Florida
 */
-use AdventureWorks2012_Data;
+
+USE AdventureWorks2012_Data;
 
 /*  first (complexity is more) solutiON */
-use AdventureWorks2012_Data;
-SELECT  pp.FirstName+' '+pp.MiddleName+' '+pp.LAStName AS PersONs_live_in_Florida
-FROM    Person.Person AS pp , Person.BusinessEntityAddress AS pb, Person.Address AS pa 
+SELECT  CONCAT(pp.FirstName,' ',pp.MiddleName,' ',pp.LAStName) AS Persons_live_in_Florida
+FROM    PersON.PersON AS pp , PersON.BusinessEntityAddress AS pb , PersON.Address AS pa 
 WHERE   (pa.AddressLine1 LIKE '%Florida%' AND 
          pp.BusinessEntityID=pb.BusinessEntityID AND 
              pb.AddressID= pa.AddressID );
 
+ 
 
-
-/*   (Best) SolutiON  */
-SELECT		pp.FirstName+' '+pp.MiddleName+' '+pp.LAStName AS PersONs_live_in_Florida 
+/*   Second SolutiON  */
+SELECT	CONCAT(pp.FirstName,' ',pp.MiddleName,' ',pp.LAStName) AS PersONs_live_in_Florida 
 FROM		PersON.PersON AS pp 
-WHERE       pp.BusinessEntityID in (SELECT  pb.AddressID 
-                                    FROM    Person.BusinessEntityAddress AS pb
-                                    WHERE   pb.BusinessEntityID in 
+WHERE       pp.BusinessEntityID IN (SELECT   pb.AddressID 
+                                    FROM     PersON.BusinessEntityAddress AS pb
+                                    WHERE    pb.BusinessEntityID IN 
 								       (SELECT  pa.AddressID 
-									FROM    PersON.Address AS pa 
-                                                                        WHERE   pa.AddressLine1 LIKE  '%Florida%' ));
+									    FROM    PersON.Address AS pa 
+                                        WHERE pa.AddressLine1 LIKE  '%Florida%' ));
 
 
+/*   THIRD SOLUTION  ,GOOD ONE    */
+
+SELECT  CONCAT(pp.FirstName,' ',pp.MiddleName,' ',pp.LAStName) AS Persons_live_in_Florida
+FROM    Person.Person AS pp INNER JOIN Person.BusinessEntityAddress AS pb 
+ON      (pp.BusinessEntityID=pb.BusinessEntityID)
+        INNER JOIN  Person.Address AS pa 
+ON      (pb.AddressID= pa.AddressID) 
+WHERE   (pa.AddressLine1 LIKE '%Florida%');
+
+
+/* USED  TABLES ..................................................... 
+select * from PersON.PersON
+select * from Person.BusinessEntityAddress
+select * from Person.Address;
+*/
    
 /* query 10.
 10. For each order show the SalesOrderID AND SubTotal calculated two ways:
@@ -127,7 +161,14 @@ WHERE       pp.BusinessEntityID in (SELECT  pb.AddressID
       B) Sum of OrderQty*UnitPrice
          All above must be in 1 query.
 		 */
-     I did not get the meaning of this query; 
+
+       /*     I did not get the meaning of this QUESTION;    */ 
+
+SELECT   ssh.SalesOrderID,ssh.SubTotal, (ssd.OrderQty*ssd.UnitPrice) AS ANSWER_B
+FROM     Sales.SalesOrderHeader ssh, Sales.SalesOrderDetail ssd,Production.Product pp
+WHERE    ssh.SalesOrderID = ssd.SalesOrderID AND pp.ProductID = ssd.ProductID
+GROUP BY ssh.SalesOrderID;
+
 
 
 /* query 11
@@ -139,11 +180,11 @@ FROM    Sales.SalesOrderDetail
 WHERE   OrderQty=1 ;
 
 
-
-
 /*query 12
 Show the product descriptiON for culture 'fr' for product with ProductID 736.
 */
+
+
 SELECT  ppd.DescriptiON AS descriptiON_for_culture_fr_AND_ProductID_736
 FROM    Production.ProductDescription AS ppd 
         INNER JOIN Production.ProductModelProductDescriptionCulture AS ppmc
@@ -152,13 +193,20 @@ ON      (ppmc.ProductDescriptionid = ppd.ProductDescriptionid)
 ON      (pp.ProductModelID = ppmc.ProductModelID)
 WHERE   (ppmc.cultureID = 'fr' AND pp.ProductID = 736);
 
+
+
+/* USED  TABLES ..................................................... 
 select * from Production.ProductDescription;
 select * from Production.ProductModelProductDescriptionCulture where CultureID='fr';
-select * from Production.Product where ProductID=736;
+select * from Production.Product where ProductID=736;*/
+
+
  
 /* query 13
     Show OrderQty, the Name AND the ListPrice of the order made by CustomerID 635
 */
+
+
 SELECT  ssd.OrderQty, pp.Name, pp.ListPrice
 FROM    Sales.SalesOrderHeader AS ssh
         INNER JOIN Sales.SalesOrderDetail AS ssd
@@ -167,14 +215,17 @@ ON      (ssd.SalesOrderID = ssh.SalesOrderID)
 ON      (ssd.ProductID=pp.ProductID)
 WHERE   ssh.CustomerID=635;
 
+
+/* USED  TABLES .....................................................
+ 
 select * from Sales.SalesOrderHeader  where CustomerID=635;
 select * from sales.SalesOrderDetail ;
-select * from Production.Product;
+select * from Production.Product;         */
 
 /* query 14.
  How many products in ProductSubCategory 'Cranksets' have been sold to an address in 'LONdON'?
 */
-/*............ .......*/
+
 
 
 SELECT  COUNT(ssd.ProductID) AS total_Cranksets_sold_in_London
@@ -189,19 +240,20 @@ ON      (pp.ProductID = ssd.ProductID)
 ON      (ppc.ProductCategoryID = pp.ProductSubcategoryID)
 WHERE   (pa.City = 'London' AND ppc.Name = 'Cranksets');
 
+
+/* USED  TABLES ..................................................... 
 select * from Sales.SalesOrderHeader;
 select * from Sales.SalesOrderDetail;
 select * from Production.ProductCategory;
 select * from Person.Address;
+select * from Production.Product;*/
 
 
 
 
 
 
-/*..........................................................*/
-
-
+/*...............  ***         END       ***  ..... */
 
 /*  For finding the data           */
 SELECT Table_Name, Column_Name 
